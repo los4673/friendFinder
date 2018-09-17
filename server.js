043e8c27a27c41ea604608fac93ec8ac10abbfc7
los4673 <35182425+los4673@users.noreply.github.com>
2018-09-16 18:10:29 -0700

    file set up and server set up

---
+++ server.js
@@ -0,0 +1,41 @@
+// Require dependencies
+var http = require("http");
+var fs = require("fs");
+
+// Set our port to 8080
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+
+  // Capture the url the request is made to
+  var path = req.url;
+
+  // When we visit different urls, read and respond with different files
+  switch (path) {
+
+    //first case
+    // case "/food":
+    // return fs.readFile(__dirname + "/food.html", function(err, data) {
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.end(data);
+    // }); // case "/food":
+    // return fs.readFile(__dirname + "/food.html", function(err, data) {
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.end(data);
+    // });
+
+    // default to rendering index.html, if none of above cases are hit
+  default:
+    return fs.readFile(__dirname + "/app/public/home.html", function(err, data) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+}
+
+// Starts our server.
+server.listen(PORT, function() {
+  console.log("Server is listening on PORT: " + PORT);
+});
